085b53a2772c5dec0807512f4586bff153634eb3
Kevin Kim <kevinnkimm96@gmail.com>
2025-03-02 21:08:46 +0900

    chore: remove mongodb models

--- models/User.ts
+++
@@ -1,9 +0,0 @@
-import { Schema, model, models } from 'mongoose'
-
-const UserSchema = new Schema({
-  number: { type: String, unique: true, required: true },
-  name: { type: String, required: true },
-  isAdmin: { type: Boolean, default: false },
-})
-
-export default models.User || model('User', UserSchema)
